646ff4dc9d9f65a27065fc80d38ba172041593ee
r_ <jayyc818@gmail.com>
2024-06-07 17:24:07 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -50,5 +50,3 @@ AddEventHandler('onResourceStart', function(resource)
 end)
 
 print('ServerSide Is Loaded [r_energydrinks, r_scripts gives you wings.]')
-print('I put an energy drink in my coffee this morning.')
-print('I got halfway to work and realized I forgot my car.')
