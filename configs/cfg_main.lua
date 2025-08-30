01df524e58622bf2abc8e58cbb3d311d6e0823c9
rumaier <148654629+rumaier@users.noreply.github.com>
2025-08-30 16:21:01 -0500

    refactor: update cfg_main.lua to

--- configs/cfg_main.lua
+++ configs/cfg_main.lua
@@ -1,9 +1,10 @@
---    _           _ _                 _       _
---   | |__   ___ (_) | ___ _ __ _ __ | | __ _| |_ ___
---   | '_ \ / _ \| | |/ _ \ '__| '_ \| |/ _` | __/ _ \
---   | |_) | (_) | | |  __/ |  | |_) | | (_| | ||  __/
---   |_.__/ \___/|_|_|\___|_|  | .__/|_|\__,_|\__\___|
---                             |_|
+--                                          _      _       _
+-- _ __  ___ _ __   ___ _ __ __ _ _   _  __| |_ __(_)_ __ | | _____
+-- | '__|/ _ \ '_ \ / _ \ '__/ _` | | | |/ _` | '__| | '_ \| |/ / __|
+-- | |  |  __/ | | |  __/ | | (_| | |_| | (_| | |  | | | | |   <\__ \
+-- |_|___\___|_| |_|\___|_|  \__, |\__, |\__,_|_|  |_|_| |_|_|\_\___/
+--  |_____|                  |___/ |___/
+--
 --  Need support? Join our Discord server for help: https://discord.gg/rscripts
 --
 Cfg = {
@@ -22,7 +23,21 @@ Cfg = {
     --  \___/| .__/ \__|_|\___/|_| |_|___/
     --       |_|
     Options = {
+        VendingMachines = {
+            Enabled = true,                        -- Determines if Junk Vending Machines spawn in the defined locations below.
+            DrinkPrice = 3,                        -- Determines the price of a Junk Energy Drink from the machines.
+            Locations = {
+                vec4(-47.87, -1090.21, 25.42, 338.36), -- Main PDM
+                vec4(324.03, -233.61, 53.22, 249.11), -- Pinkcage Motel
+                vec4(-11.89, -1478.20, 29.50, 138.79), -- Strawberry Liquor Store
+            },
+        },
 
+        Effects = {
+            StaminaBoost = true, -- Determines if energy drinks give a Stamina Boost.
+            SprintBoost = true, -- Determines if energy drinks give a Sprint Boost.
+            Duration = 60    -- Determines how long the above effects will last on a player.
+        },
     },
     --      _      _
     --   __| | ___| |__  _   _  __ _
